c8c2a61f75f694954c41b993fc3ebc1cee0c7179
Jakub Kobialka <kobialka.kontakt@gmail.com>
2024-05-12 12:28:42 +0200

    init routes config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+
 function App() {
 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/'>
+      <Route index element={<div>Login</div>} />
+      <Route path='register' element={<div>Register</div>} />
+    </Route>
+  ))
+
+
   return (
-    <div>
-      Hello world!
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
