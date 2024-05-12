6e2e844cf92a8fc8426a29cdead1784b5cf6ec87
Jakub Kobialka <kobialka.kontakt@gmail.com>
2024-05-12 13:31:41 +0200

    eslint rules and tailwind config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,5 +14,23 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ],
+    'jsx-quotes': [2, 'prefer-single'],
+    "comma-dangle": ["error", {
+      "arrays": "always",
+      "objects": "always",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never"
+    }],
+    "@typescript-eslint/no-unused-vars" : "off",
+    "semi": ["error", "always"],
   },
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 function App() {
-
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/'>
-      <Route index element={<div>Login</div>} />
+      <Route index element={<h1 className='text-3xl font-bold underline'>Login</h1>} />
       <Route path='register' element={<div>Register</div>} />
     </Route>
-  ))
+  ));
 
 
   return (
     <RouterProvider router={router} />
-  )
+  );
 }
 
-export default App
+export default App;
